2d3cb1fec77e495120b69b3774c91370126c4348
epsilonrt <epsilonrt@gmail.com>
2022-02-20 18:28:07 +0100

    Add type_name() template function

---
+++ include/type_name.h
@@ -0,0 +1,24 @@
+/**
+ * SpaIot Library (c) by espilonrt - epsilonrt@gmail.com
+ * This file is part of SpaIot library <https://github.com/epsilonrt/spaiot-lib>
+ *
+ * SpaIot library is licensed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ *
+ * SpaIot library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;
+ */
+#pragma once
+
+#include <Arduino.h>
+
+template <class T>
+String type_name (const T&) {
+  String s = __PRETTY_FUNCTION__;
+  int start = s.indexOf ("[with T = ") + 10;
+  int stop = s.lastIndexOf (']');
+  return s.substring (start, stop);
+}
---
+++ test/t05_type_name/main.cpp
@@ -0,0 +1,107 @@
+//
+// Unit Test for the type_name
+//
+#include <Arduino.h>
+#include <unity.h>
+#include <type_name.h>
+
+// void setUp(void) {
+// // set stuff up here
+// }
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+// Abstract class with ==operator
+class A {
+
+  public:
+    virtual ~A() {}
+    virtual void doit() = 0;
+    virtual bool operator== (const A &other) const {
+
+      return type_name (*this) == type_name (other);
+    }
+    virtual bool operator!= (const A &other) const {
+
+      return ! (*this == other);
+    }
+};
+
+// Derived class
+class B : public A {
+
+  public:
+    B (int i = 0) : m_int (i) {}
+    void doit() {}
+    virtual bool operator== (const A &other) const {
+
+      if (A::operator== (other)) {
+
+        return m_int == static_cast<const B&> (other).m_int;
+      }
+      return false;
+    }
+    void set (int i) {
+      m_int = i;
+    }
+  private:
+    int m_int;
+};
+
+void test_type_name (void) {
+  B b;
+  A &a = b;
+
+  String message = "type_name(b) = " + type_name (b) + " --- type_name(a) = " + type_name (a);
+  TEST_PASS_MESSAGE (message.c_str());
+  TEST_ASSERT (type_name (b) == "B");
+  TEST_ASSERT (type_name (a) == "A");
+}
+
+void test_equal (void) {
+  B b1;
+  B b2;
+  A &a1 = b1;
+  A &a2 = b2;
+  TEST_ASSERT (b1 == b2);
+  TEST_ASSERT (a1 == b2);
+  TEST_ASSERT (b1 == a2);
+  TEST_ASSERT (a1 == a2);
+}
+
+void test_notequal (void) {
+  B b1 (1);
+  B b2 (2);
+  A &a1 = b1;
+  A &a2 = b2;
+  TEST_ASSERT (b1 != b2);
+  TEST_ASSERT (a1 != b2);
+  TEST_ASSERT (b1 != a2);
+  TEST_ASSERT (a1 != a2);
+}
+
+void setup() {
+
+  // NOTE!!! Wait for >2 secs
+  // if board doesn't support software reset via Serial.DTR/RTS
+  delay (2000);
+
+  UNITY_BEGIN();    // IMPORTANT LINE!
+}
+
+bool done = false;
+
+void loop() {
+
+  if (!done) {
+
+    RUN_TEST (test_type_name);
+    RUN_TEST (test_equal);
+    RUN_TEST (test_notequal);
+
+    UNITY_END(); // stop unit testing
+    done = true;
+  }
+}

1b79f6377d0dfcb251c6f6355f8be0833403ae01
epsilonrt <epsilonrt@gmail.com>
2022-02-21 09:40:12 +0100

    Update SpaSimple example

--- README.md
+++ README.md
@@ -112,7 +112,7 @@ const char ConfigName[] = "SCIP2SSP";
 const unsigned long TimerTime = (12 * 60 * 60); // 12 hours in seconds
 const unsigned long SerialBaudrate = 115200;
 
-ControlPanel * panel; // pointer on the control panel
+ControlPanel * spa; // pointer on the control panel
 unsigned long timer;
 uint16_t waterTemp;
 
@@ -122,16 +122,20 @@ void setup() {
   Serial.begin (SerialBaudrate);
   Serial.println ("\nSpaIot Simple Example");
 
-  panel = ControlPanel::getInstance (ConfigName);
-  if (panel == nullptr) { // check if the requested configuration has been found
+  spa = ControlPanel::getInstance (ConfigName);
+  if (spa == nullptr) { // check if the requested configuration has been found
     Serial.println ("Harware settings not found");
-    exit(EXIT_FAILURE);
+    for(;;); // loop always, to stop 
   }
 
-  panel->begin();  // IMPORTANT LINE!
+  spa->begin();  // IMPORTANT LINE!
+  if (spa->isOpened() == false) { // check if the connection to the spa has been open
+    Serial.println ("No spa connection found");
+    for(;;); // loop always, to stop 
+  }
   
   // Wait to read the temperature of the water, it can take 20 seconds ...
-  waterTemp = panel->waitForWaterTemp();
+  waterTemp = spa->waitForWaterTemp();
   Serial.printf ("waterTemp=%d'C\n", waterTemp);
   Serial.printf ("Waiting to reset filter every %lu seconds...", TimerTime);
 }
@@ -139,20 +143,19 @@ void setup() {
 
 void loop() {
 
-  uint16_t t = panel->waterTemp(); // Reading the temperature of the water
+  uint16_t t = spa->waterTemp(); // Reading the temperature of the water
   if (waterTemp != t) { // If modified, it is displayed
     waterTemp = t;
     Serial.printf ("waterTemp=%d'C\n", waterTemp);
   }
 
   if (timer >= TimerTime) { // If the time has been reached, 
-    if (panel->isFilterOn()) { // you check if the filtering is running, 
-
+    if (spa->isFilterOn()) { // you check if the filtering is running, 
       Serial.println ("I rearm the spa filter");
       // if so, it is stopped and restarts.
-      panel->setFilter (false);
+      spa->setFilter (false);
       delay (1000);
-      panel->setFilter (true);
+      spa->setFilter (true);
     }
   }
 
--- examples/SpaSimple/src/main.cpp
+++ examples/SpaSimple/src/main.cpp
@@ -1,6 +1,9 @@
 //
 // SpaIot Simple Example
 //
+// This example displays the temperature of the water and resets the filtration 
+// of the spa, if it is started, to prevent the filtration from stopping 
+// after 24 hours.
 #include <Arduino.h>
 #include <SpaIot.h>
 
@@ -12,7 +15,7 @@ const char ConfigName[] = "SCIP2SSP";
 const unsigned long TimerTime = (12 * 60 * 60); // 12 hours in seconds
 const unsigned long SerialBaudrate = 115200;
 
-ControlPanel * panel; // pointer on the control panel
+ControlPanel * spa; // pointer on the control panel
 unsigned long timer;
 uint16_t waterTemp;
 
@@ -22,16 +25,20 @@ void setup() {
   Serial.begin (SerialBaudrate);
   Serial.println ("\nSpaIot Simple Example");
 
-  panel = ControlPanel::getInstance (ConfigName);
-  if (panel == nullptr) { // check if the requested configuration has been found
+  spa = ControlPanel::getInstance (ConfigName);
+  if (spa == nullptr) { // check if the requested configuration has been found
     Serial.println ("Harware settings not found");
-    exit(EXIT_FAILURE);
+    for(;;); // loop always, to stop 
   }
 
-  panel->begin();  // IMPORTANT LINE!
+  spa->begin();  // IMPORTANT LINE!
+  if (spa->isOpened() == false) { // check if the connection to the spa has been open
+    Serial.println ("No spa connection found");
+    for(;;); // loop always, to stop 
+  }
   
   // Wait to read the temperature of the water, it can take 20 seconds ...
-  waterTemp = panel->waitForWaterTemp();
+  waterTemp = spa->waitForWaterTemp();
   Serial.printf ("waterTemp=%d'C\n", waterTemp);
   Serial.printf ("Waiting to reset filter every %lu seconds...", TimerTime);
 }
@@ -39,20 +46,20 @@ void setup() {
 
 void loop() {
 
-  uint16_t t = panel->waterTemp(); // Reading the temperature of the water
+  uint16_t t = spa->waterTemp(); // Reading the temperature of the water
   if (waterTemp != t) { // If modified, it is displayed
     waterTemp = t;
     Serial.printf ("waterTemp=%d'C\n", waterTemp);
   }
 
   if (timer >= TimerTime) { // If the time has been reached, 
-    if (panel->isFilterOn()) { // you check if the filtering is running, 
+    if (spa->isFilterOn()) { // you check if the filtering is running, 
 
       Serial.println ("I rearm the spa filter");
       // if so, it is stopped and restarts.
-      panel->setFilter (false);
+      spa->setFilter (false);
       delay (1000);
-      panel->setFilter (true);
+      spa->setFilter (true);
     }
   }
 

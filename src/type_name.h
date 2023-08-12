e3608ec43126c6b9b8a3f42b2143516b7f1b2080
epsilonrt <epsilonrt@gmail.com>
2023-08-13 00:45:33 +0200

    doc: document type_name

--- src/type_name.h
+++ src/type_name.h
@@ -1,25 +1,30 @@
 /*
- * SpaIot Library (c) by epsilonrt - epsilonrt@gmail.com
- * This file is part of SpaIot library <https://github.com/epsilonrt/spaiot-lib>
- *
- * SpaIot library is licensed under a
- * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
- *
- * You should have received a copy of the license along with this
- * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
- *
- * SpaIot library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;
- */
+   SpaIot Library (c) by epsilonrt - epsilonrt@gmail.com
+   This file is part of SpaIot library <https://github.com/epsilonrt/spaiot-lib>
+
+   SpaIot library is licensed under a
+   Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+   You should have received a copy of the license along with this
+   work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+
+   SpaIot library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY;
+*/
 #pragma once
 
 #include <Arduino.h>
 
 /**
- *  A template function that returns the type name of the parameter. 
- */
+    @brief A template function that returns the type name of the parameter.
+
+    typeid() is not used because it is not supported by all platforms (with -fno-rtti enabled).
+    for example, the ESP32 compiler does not support it:
+    > error: cannot use 'typeid' with -fno-rtti
+
+*/
 template <class T>
-String type_name (const T&) {
+String type_name (const T &) {
   String s = __PRETTY_FUNCTION__;
   int start = s.indexOf ("[with T = ") + 10;
   int stop = s.lastIndexOf (']');
